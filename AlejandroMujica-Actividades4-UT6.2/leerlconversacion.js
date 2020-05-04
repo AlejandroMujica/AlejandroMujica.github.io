09796f2ad6519ad0c660f2fa8b8ba89aff6452ec
AlejandroMujica <57433556+AlejandroMujica@users.noreply.github.com>
2020-05-04 22:09:34 +0200

    First commit

---
+++ AlejandroMujica-Actividades4-UT6.2/leerlconversacion.js
@@ -0,0 +1,15 @@
+function gestionarFicheroXML(xmlDoc){
+	
+	alert(xmlDoc.getElementsByTagName('libro')[0].firstChild.nodeValue)
+	alert(xmlDoc.getElementsByTagName('libreria')[0].childNodes[1].firstChild.nodeValue)
+
+    document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(2)').textContent +
+                                                               xmlDoc.querySelector('libro:nth-child(3)').textContent +
+                                                               xmlDoc.querySelector('libro:nth-child(4)').textContent +"</b>"
+	
+}
+
+document.querySelector("div:nth-child(1)").addEventListener("click",()=>{
+
+    let xmlDoc = loadLDocA("conversacion.txt","txt");
+})
