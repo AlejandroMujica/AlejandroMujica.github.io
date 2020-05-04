669ad6a8adc0200845148fcbb8759e934bdab0f5
AlejandroMujica <57433556+AlejandroMujica@users.noreply.github.com>
2020-05-04 21:49:13 +0200

    Update leerlibros.js

--- AlejandroMujica-Actividades4-UT6/leerlibros.js
+++ AlejandroMujica-Actividades4-UT6/leerlibros.js
@@ -1,11 +1,16 @@
 function gestionarFicheroXML(xmlDoc){
 	
-	alert(xmlDoc.getElementsByTagName('libro')[0].firstChild.nodeValue)
 	alert(xmlDoc.getElementsByTagName('libreria')[0].childNodes[1].firstChild.nodeValue)
 
     document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(2)').textContent +
                                                                xmlDoc.querySelector('libro:nth-child(3)').textContent +
                                                                xmlDoc.querySelector('libro:nth-child(4)').textContent +"</b>"
+
+    alert(xmlDoc.getElementsByTagName('libreria')[1].childNodes[1].firstChild.nodeValue)
+
+    document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(5)').textContent +
+                                                               xmlDoc.querySelector('libro:nth-child(6)').textContent +
+                                                               xmlDoc.querySelector('libro:nth-child(7)').textContent +"</b>"
 	
 }
 
