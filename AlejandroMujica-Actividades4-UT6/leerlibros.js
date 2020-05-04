55be0265254954c10be8d7aa32f2cc04f846eae2
AlejandroMujica <57433556+AlejandroMujica@users.noreply.github.com>
2020-05-04 21:54:44 +0200

    Update leerlibros.js

--- AlejandroMujica-Actividades4-UT6/leerlibros.js
+++ AlejandroMujica-Actividades4-UT6/leerlibros.js
@@ -1,14 +1,14 @@
 function gestionarFicheroXML(xmlDoc){
 	
-	alert(xmlDoc.getElementsByTagName('libreria')[0].childNodes[1].firstChild.nodeValue)
+    alert(xmlDoc.getElementsByTagName('libreria')[0].childNodes[1].firstChild.nodeValue)
+    alert(xmlDoc.getElementsByTagName('libreria')[1].childNodes[1].firstChild.nodeValue)
 
-    document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(2)').textContent +
+    document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('nombre:nth-child(1)').textContent +
+                                                               xmlDoc.querySelector('libro:nth-child(2)').textContent +
                                                                xmlDoc.querySelector('libro:nth-child(3)').textContent +
-                                                               xmlDoc.querySelector('libro:nth-child(4)').textContent +"</b>"
-
-    alert(xmlDoc.getElementsByTagName('libreria')[1].childNodes[2].firstChild.nodeValue)
-
-    document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(5)').textContent +
+                                                               xmlDoc.querySelector('libro:nth-child(4)').textContent +
+                                                               xmlDoc.querySelector('nombre:nth-child(2)').textContent +
+                                                               xmlDoc.querySelector('libro:nth-child(5)').textContent +
                                                                xmlDoc.querySelector('libro:nth-child(6)').textContent +
                                                                xmlDoc.querySelector('libro:nth-child(7)').textContent +"</b>"
 	
